553ac5d831d4f8fb0756bb7ddfe1f7ba92011682
Gedeon DUFITIMANA <gedeon.dufitimana2023@kepler.org>
2026-01-01 12:25:40 +0200

    Add test to repeat test N times

---
+++ src/test/java/BankAccountRepeatedTestTests.java
@@ -0,0 +1,24 @@
+import bank.BankAccount;
+import dependencies_injection.BankAccountParameterResolver;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@ExtendWith(BankAccountParameterResolver.class)
+public class BankAccountRepeatedTestTests {
+    @RepeatedTest(6)
+    @DisplayName("Deposit 500 successfully")
+    public void testDeposit(BankAccount bankAccount){
+        bankAccount.deposit(500);
+        assertEquals(500,bankAccount.getBalance());
+    }
+
+    @RepeatedTest(6)
+    @DisplayName("Deposit 500 successfully")
+    public void testDepositRepetitionInfo(BankAccount bankAccount, RepetitionInfo repetitionInfo){
+        bankAccount.deposit(500);
+        assertEquals(500,bankAccount.getBalance());
+        System.out.println("Nr: "+repetitionInfo.getCurrentRepetition());
+    }
+}
