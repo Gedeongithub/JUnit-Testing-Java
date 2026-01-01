ec6eb939bfa2040eca553f16a8dad7313c3aa222
Gedeon DUFITIMANA <gedeon.dufitimana2023@kepler.org>
2026-01-01 16:46:03 +0200

    Add a test with Beforeall and Afterall annotations

---
+++ src/test/java/BankAccountBeforeAndAfterTest.java
@@ -0,0 +1,31 @@
+import bank.BankAccount;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class BankAccountBeforeAndAfterTest {
+
+      BankAccount bankAccount;
+     @BeforeAll
+     public void runBefore(){
+         bankAccount = new BankAccount(500,0);
+     }
+
+    @Test
+    void testWithdraw() {
+        bankAccount.withdraw(300);
+        assertEquals(200, bankAccount.getBalance());
+    }
+
+    @Test
+    void testDeposit() {
+        bankAccount.deposit(500);
+        assertEquals(700, bankAccount.getBalance());
+    }
+
+    @AfterAll
+    public void tearDown(){
+        System.out.println("Bye");
+    }
+}
